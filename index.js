2da498fd060d741ef5570e2f2a8bd8efb4113916
TabithaKoech <tabbyk1718@gmail.com>
2022-06-13 17:40:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('text').textContent='This is really cool!';
+    
+
+   
+    });
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
